60ad2589c2fcb0735b074be3887fd30c4459e4e8
msheehan1720 <86561003+msheehan1720@users.noreply.github.com>
2021-07-03 13:45:13 -0400

    Use /tmp

--- config/configure.sh
+++ config/configure.sh
@@ -11,17 +11,17 @@ stop_server()
 }
 
 echo "Setting Password"
-/work/set_password.sh
+/tmp/work/set_password.sh
 start_server
 PID=$(ps -C java -o pid= | tr -d " ")
 echo "Applying configuration"
 if [ ! -z "$1" ]; then
-    /work/run_py_script.sh "$@"
-elif [ ! -z "$(ls /work/config)" ]; then
-    echo "+ Found config-files under /work/config. Executing..."
-    find /work/config -name "*.py"  -print0 | sort -z | xargs -0 -n 1 -r /work/run_py_script.sh
+    /tmp/work/run_py_script.sh "$@"
+elif [ ! -z "$(ls /tmp/work/config)" ]; then
+    echo "+ Found config-files under /tmp/work/config. Executing..."
+    find /tmp/work/config -name "*.py"  -print0 | sort -z | xargs -0 -n 1 -r /tmp/work/run_py_script.sh
 fi
-work/applyConfig.sh
+/tmp/work/applyConfig.sh
 stop_server
 find /opt/IBM -user was ! -perm -g=w -print0 | xargs -0 -r chmod g+w
 find /opt/IBM -type d -user was ! -perm -g=x -print0 | xargs -0 -r chmod g+x
